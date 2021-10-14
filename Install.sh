78de99e5c39627903e2c1bc3913355d5e5bbe739
MATAR0U <85929482+MATAR0U@users.noreply.github.com>
2021-10-14 13:59:56 +0200

    Update Install.sh

--- Install.sh
+++ Install.sh
@@ -1,7 +1,4 @@
-#!/bin/sh
-
-#Definition de la variable $CFG
-CFG='$CFG'
+#!/bin/bash
 
 #Verification de l'utilisateur
 if [ "$USER" = "root" ]; then
@@ -11,13 +8,8 @@ else
     exit
 fi
 
-#Definition de la boucle pour choisir son interface reseau
-boucleip(){
-    clear
-    ip a
-    echo -n "\nQuel est ton interface reseau utiliser ? "
-    read interfaces
-}
+#Definition de la variable $CFG
+CFG='$CFG'
 
 #Definition de la boucle pour choisir ou non de securiser le site en HTTPS
 boucleSSL(){
@@ -38,64 +30,139 @@ boucleInstall(){
 #Definition de la boucle Moodle
 boucleMoodle(){
 		
-		
-	#Definition de la boucle pour choisir un nom pour la base de donnee
-	boucleDATABASE(){
-		echo -n "\nChoisi un nom pour la base de donnee : "
-		read dataname
-		echo -n "Etes vous sur de choisir $dataname (O/n) ? "
-		read sure
-		if [ "$sure" = "o" -o "$sure" = "O" -o "$sure" = "" ]; then
-			echo "Le nom de votre base de donniee sera $dataname"
-		elif [ "$sure" = "n" -o "$sure" = "N" ]; then
-			boucleDATABASE
-		fi
-	}
-		
-	#Definition de la boucle pour le nom d'utilisateur de la base de donnee
-	boucleUSER(){
-		echo -n "\nChoisi un nom d'utilisateur pour la base de donnee $dataname : "
-		read datauser
-		echo -n "Etes vous sur de choisir $datauser (O/n) ? "
-		read sure
-		if [ "$sure" = "o" -o "$sure" = "O" -o "$sure" = "" ]; then
-			echo "Le nom d'utilisateur pour la base de donnee $dataname sera $datauser"
-		elif [ "$sure" = "n" -o "$sure" = "N" ]; then
-			boucleUSER
-		else
-			boucleUSER
-		fi
-	}
-		
-	#Definition de la boucle pour le mot de passe de l'utilisateur de la base de donnee
-	bouclePASSWD(){
-		echo -n "\nChoisi un mot de passe pour l'utilisateur $datauser : "
-		read datapdw
-		echo -n "Etes vous sur de choisir $datapdw (O/n) ? "
-		read sure
-		if [ "$sure" = "o" -o "$sure" = "O" -o "$sure" = "" ]; then
-			echo "Le mot de passe de l'utilisateur $datauser de la base de donnee $dataname sera $datapdw"
-		elif [ "$sure" = "n" -o "$sure" = "N" ]; then
-			bouclePASSWD
-		else
-			bouclePASSWD
-		fi
-	}
-		
 	#Lancement des boucles
-	boucleip
+	
+	obligatoire=0
+	
+	if [ "$1" = "-h" ] || [ "$1" = "--help" ]
+	then
+		if [[ "$2" =~ [A-Za-z0-9]* ]] || [ "$2" = "" ]
+		then
+			echo ""
+			echo "Voici les parametres :"
+			echo "-h OU --help : affiche les aides"
+			echo "[OBLIGATOIRE] -d OU --data-base : specifie le nom de la base de donnees"
+			echo "[OBLIGATOIRE] -u OU --user: specifie le nom de l'utitilisateur"
+			echo "[OBLIGATOIRE] -p OU --password : specifie le mot de passe de l'utilisateur de la base de donnee"
+			echo "[OBLIGATOIRE] -i OU --ip : specifie l'interface reseaux a utiliser OU l'adresse IP (v4 ou v6)"
+			echo ""
+			exit
+		else
+			echo "Parametre incorrect pour "$1
+			echo "Utiliser l'option -h ou --help pour afficher les options"
+		fi
+	fi
+	
+	number=$((($#/2)+1))
+	
+	while [ "$number" -gt 0 ]
+	do
+		if [ "$1" = "-d" ] || [ "$1" = "--data-base" ]
+		then
+			if [[ "$2" =~ [A-Za-z0-9]* ]]
+			then
+				dataname=$2
+				obligatoire=$(($obligatoire+1))
+				shift
+				shift
+			else
+				echo ""
+				echo "Parametre incorrect pour "$1
+				echo "Utiliser l'option -h ou --help pour afficher les options"
+				echo ""
+				exit
+			fi
+		fi
+	
+		if [ "$1" = "-u" ] || [ "$1" = "--user" ]
+		then
+			if [[ "$2" =~ [A-Za-z0-9]* ]]
+				then
+					datauser=$2
+					obligatoire=$(($obligatoire+1))
+					shift
+					shift
+				else
+					echo ""
+					echo "Parametre incorrect pour "$1
+					echo "Utiliser l'option -h ou --help pour afficher les options"
+					echo ""
+					exit
+				fi
+		fi
+	
+		if [ "$1" = "-p" ] || [ "$1" = "--password" ]
+		then
+			datapdw=$2
+			obligatoire=$(($obligatoire+1))
+			shift
+			shift
+		fi
+	
+		if [ "$1" = "--ip" ]
+		then
+			if [[ "$2" =~ [a-zA-Z0-9]* ]] || [ "$2" != "" ]
+			then
+				ip=$2
+				obligatoire=$(($obligatoire+1))
+				shift 2
+			else
+				echo ""
+				echo "Parametre incorrect pour "$1
+				echo "Utiliser l'option -h ou --help pour afficher les options"
+				echo ""
+				exit
+			fi
+		fi
+		number=$(($number-1))
+	
+		if [ "$1" = "-i" ]
+		then
+			if [[ "$2" =~ [a-zA-Z0-9]* ]] || [ "$2" != "" ]
+			then
+				interfaces=$2
+				ip=$(ifconfig $interfaces | awk '/inet / {print $2}' | cut -d ':' -f2)
+				obligatoire=$(($obligatoire+1))
+				shift 2
+			else
+				echo ""
+				echo "Parametre incorrect pour "$1
+				echo "Utiliser l'option -h ou --help pour afficher les options"
+				echo ""
+				exit
+			fi
+		fi
+		number=$(($number-1))
+	done
+	if [ "$obligatoire" -lt 4 ]
+	then
+		echo ""
+		echo "Certaines options obligatoire ne sont pas renseigner correctement ($obligatoire sur 4)"
+		echo "Utiliser l'option -h ou --help pour afficher les options"
+		echo ""
+		exit
+	fi
+	
+	echo ""
+	echo "Voici un resumer des informations"
+	echo ""
+	echo "Nom de la base de donnees : $dataname"
+	echo "Nom de l'utilisateur de la base de donnees : $datauser"
+	echo "Mot de passe de l'utilisateur de la base de donnees: $datapdw"
+	echo "Adresse IP avec laquel sera configurer le serveur : $ip"
+	echo ""
+	read -p "Ces informations sont-elles correct ? (o/N) : " valide
+	if [ "$valide" = "n" ] || [ "$valide" = "N" ] || [ "$valide" = "" ]
+	then
+		exit
+	fi
+	
 	echo -n "Faut il generer une cle SSL (o/N) ? "
 	read ssl
 	if [ "$ssl" = "o" -o "$ssl" = "O" ]; then
 		boucleSSL
 	fi
-	boucleDATABASE
-	boucleUSER
-	bouclePASSWD
 	boucleInstall
-		
-	#Recuperation de l'ip
-	ip=$(ifconfig $interfaces | awk '/inet / {print $2}' | cut -d ':' -f2)
 		
 	#Creation et configuration de la base de donnee
 	mysql -u root -e "CREATE DATABASE IF NOT EXISTS $dataname CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;"
